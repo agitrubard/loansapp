beeeee628b20ebba38d4b6237e87d26f1efab559
agitrubard <demiragitrubar@gmail.com>
2020-11-02 17:54:15 +0300

    LoansApp

---
+++ src/main/java/com/agitrubard/loansapp/LoansappApplication.java
@@ -0,0 +1,13 @@
+package com.agitrubard.loansapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agitrubard/loansapp/LoansappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agitrubard.loansapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
